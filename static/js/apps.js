948a75223a8e6b80666c590e4950664c7bbb7536
JulietChristine <66885451+JulietChristine@users.noreply.github.com>
2020-11-12 17:53:57 -0600

    no table

--- static/js/apps.js
+++ static/js/apps.js
@@ -23,45 +23,6 @@ d3.json("http://127.0.0.1:5000/api/v1.0/archvisdata", function(data) {
     Plotly.newPlot("bar", barPlotData, barPlotLayout);
 });
 
-d3.json("http://127.0.0.1:5000/api/v1.0/adelavisdata", function(error,data) {
-
-  function tabulate(data, columns) {
-		var table = d3.select('#books-table').append('table-active')
-		var thead = table.append('thead')
-		var	tbody = table.append('tbody');
-
-		// append the header row
-		thead.append('tr')
-		  .selectAll('th')
-		  .data(columns).enter()
-		  .append('th')
-		    .text(function (column) { return column; });
-
-		// create a row for each object in the data
-		var rows = tbody.selectAll('tr')
-		  .data(data)
-		  .enter()
-		  .append('tr');
-
-		// create a cell in each row for each column
-		var cells = rows.selectAll('td')
-		  .data(function (row) {
-		    return columns.map(function (column) {
-		      return {column: column, value: row[column]};
-		    });
-		  })
-		  .enter()
-		  .append('td')
-		    .text(function (d) { return d.value; });
-
-	  return table;
-	}
-
-	// render the table(s)
-	tabulate(data, ['author', 'descriptions']); // 2 column table
-
-});
-
 let svgWidth = 1200;
 let svgHeight = 600;
 
